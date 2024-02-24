56797be07a5d8fffa427cb1c706cf4badf6714f8
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-02-25 02:14:44 +0600

    created the loggedin user persistent with redux

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -1,17 +1,42 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app);
+
+// export default auth;
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: "AIzaSyB8JTcMDCsCeLZs90GS4Te7aFXcWJ6Hsww",
+  authDomain: "taskmaster-ee154.firebaseapp.com",
+  projectId: "taskmaster-ee154",
+  storageBucket: "taskmaster-ee154.appspot.com",
+  messagingSenderId: "258311457662",
+  appId: "1:258311457662:web:c817211705a7d2911bc12f"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 
 export default auth;
